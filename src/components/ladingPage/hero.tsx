450270e8a364e8b42b54130d7c45e7dd7a05a968
AjeetkumarYadav07 <iamsupertrader07@gmail.com>
2025-07-18 01:57:04 +0530

    add types

---
+++ src/components/ladingPage/hero.tsx
@@ -0,0 +1,9 @@
+import { landingData } from "../../rowdata/landing/landingMockData";
+
+export function Hero () {
+    return (
+          <section>
+            {landingData.tagline};
+          </section>
+    )
+}
