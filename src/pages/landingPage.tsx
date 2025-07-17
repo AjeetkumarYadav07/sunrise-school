baaef82544bc386b67163d6773131dc1c88cc86e
AjeetkumarYadav07 <iamsupertrader07@gmail.com>
2025-07-18 01:25:02 +0530

    add changes

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -1,9 +1,11 @@
+import { Footer } from "../components/ladingPage/footer"
+import { Navbar } from "../components/ladingPage/navbar"
 
 const LandingPage = () => {
   return (
     <div>
-        LandingPage
-    
+    <Navbar/>
+    <Footer/>
          
     </div>
   )
