6a97fb9a22a6fab33b09828bc822bcc47cc2dd43
AjeetkumarYadav07 <iamsupertrader07@gmail.com>
2025-07-18 00:43:20 +0530

    add landing and dashboard folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import { ThemeProvider } from "./components/theme/theme-provider"
-import { ModeToggle } from "./components/theme/mode-toggle"
-
+import { ThemeProvider } from "./components/theme/theme-provider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import DashboardLayout from "./Layout/dashBoardLayout";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="min-h-screen bg-background text-foreground p-4">
-        <ModeToggle />
-        <h1 className="text-2xl font-bold mt-4">Hello TS</h1>
-     
-      </div>
-    </ThemeProvider>
-  )
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <div className="min-h-screen bg-background text-foreground p-4">
+          {/* <ModeToggle /> */}
+
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/dashboard" element={<DashboardLayout/>} > 
+              <Route />
+            </Route>
+          </Routes>
+        </div>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Layout/sideBar.tsx
@@ -0,0 +1,5 @@
+export function Sidebar () {
+    return (
+       <h1>Hi i'm sider bar</h1>
+    );
+}
