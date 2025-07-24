5edb8e6aa3fbc060df385a9897f52011facb8d71
AjeetkumarYadav07 <iamsupertrader07@gmail.com>
2025-07-24 17:37:25 +0530

    add student-panel

--- src/assets/landingImage/ladingImage.ts
+++ src/assets/landingImage/ladingImage.ts
@@ -1,7 +1,9 @@
 import classroom_img1 from './classroom-Img1.jpeg' ;
 import classroom_img2 from './classroom-Img2.jpeg' ;
+import student_img1 from './studentimg1.webp'
 
 export const ladingImages = {
     classroom_img1 ,
     classroom_img2,
+    student_img1 ,
 }
